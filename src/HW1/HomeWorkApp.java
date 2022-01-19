b5d67f0fce495558774db7382daecebe16da2d14
geekbrains <info@geekbrains.ru>
2022-01-19 15:26:37 +1100

    First step

---
+++ src/HW1/HomeWorkApp.java
@@ -0,0 +1,56 @@
+package HW1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        System.out.println("***First task***");
+        printThreeWords();
+        System.out.println("\n***Next task***");
+        checkSumSign();
+        System.out.println("\n***Next task***");
+        System.out.println(printColor(0));
+        System.out.println(printColor(1));
+        System.out.println(printColor(101));
+        System.out.println("\n***Next task***");
+        System.out.println(compareNumbers(1,15));
+        System.out.println(compareNumbers(15,1));
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 5;
+        int b= -7;
+        int sum = a + b;
+        if (sum >= 0){
+            System.out.println("Sum is positive.\n");
+        } else{
+            System.out.print("Sum is negative.\n");
+        }
+
+    }
+
+    public static String printColor(int value){
+        if (value <= 0){
+            return "Red";
+        }else if(value > 0 && value <= 100){
+            return "Yellow";
+        }else {
+            return "Green";
+        }
+    }
+
+    public static String compareNumbers(int a, int b) {
+        if (a >= b) {
+            return "a >= b";
+        } else {
+            return "a < b";
+        }
+    }
+}
+
+
